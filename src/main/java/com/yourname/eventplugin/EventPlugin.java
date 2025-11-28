bd336fd67cc8530e23cf600db2ea4503bbdb0ec7
LiinCoding <liingaming@gmail.com>
2025-11-28 20:42:06 +0100

    Create EventPlugin.java

---
+++ src/main/java/com/yourname/eventplugin/EventPlugin.java
@@ -0,0 +1,28 @@
+package com.yourname.eventplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EventPlugin extends JavaPlugin {
+
+    private static EventPlugin instance;
+    private EventManager eventManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        saveDefaultConfig();
+
+        eventManager = new EventManager(this);
+
+        getCommand("event").setExecutor(new commands.EventCommand(eventManager));
+    }
+
+    public static EventPlugin getInstance() {
+        return instance;
+    }
+
+    public EventManager getEventManager() {
+        return eventManager;
+    }
+}
